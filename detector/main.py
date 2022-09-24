d0e3feaaac66fb5cecdc66944286663e190bc489
chilaxan <chilaxan@gmail.com>
2022-09-24 02:55:40 -0400

    work on detector

--- detector/main.py
+++ detector/main.py
@@ -5,8 +5,7 @@ import requests
 import urllib.parse
 import speech_recognition as sr
 import pyaudio
-import time
-import threading
+import PySimpleGUI as sg
 
 API_URL = 'http://chilaxan.tech/{user}/{device}/{action}'
 PASSWORD = 'best-password-ever'
@@ -16,8 +15,20 @@ rec = sr.Recognizer()
 rec.dynamic_energy_threshold = False
 rec.energy_threshold = 400
 
+layout = [[sg.VPush(background_color = None)],
+          [sg.Text('No One Detected', key='-OUTPUT-', font='Any 30', pad=(0,0))],
+          [sg.VPush(background_color = None)],
+          [sg.Button('Quit', font='Any 20', pad=(0,0))],
+          [sg.VPush(background_color = None)]]
+
 def main():
     video_capture = cv2.VideoCapture(0)
+    window = sg.Window(
+        'Detector',
+        layout,
+        element_justification='c',
+        size=(800,400)
+    ).Finalize()
     known_encodings = []
     known_users = []
     for img in os.listdir('users'):
@@ -31,6 +42,7 @@ def main():
         face_locations = face_recognition.face_locations(small_frame, model="cnn")
         face_encodings = face_recognition.face_encodings(small_frame, face_locations)
         # Display the results
+
         names = []
         for face_encoding in face_encodings:
             matches = face_recognition.compare_faces(known_encodings, face_encoding)
@@ -40,50 +52,41 @@ def main():
             if True in matches:
                 first_match_index = matches.index(True)
                 name = known_users[first_match_index]
-                try:
-                    do_user(name)
-                except Exception as e:
-                    if e.args[0] == 'Quit':
-                        video_capture.release()
-                h, w, c = frame.shape
-                cv2.putText(
-                    frame,
-                    f"Hey, {name.title()}, what would you like to do?",
-                    (20, int(h) - 10),
-                    cv2.FONT_HERSHEY_DUPLEX,
-                    1,
-                    (0, 255, 0),
-                    3
-                )
-
-            names.append(name)
+                names.append(name)
 
         # Display the resulting image
         for (top, right, bottom, left), name in zip(face_locations, names):
             # Scale back up face locations since the frame we detected in was scaled to 1/4 size
-            top *= 4
-            right *= 4
-            bottom *= 4
-            left *= 4
 
-            face_image = frame[top:bottom, left:right]
+            face_image = small_frame[top:bottom, left:right]
 
             # Blur the face image
             face_image = cv2.GaussianBlur(face_image, (99, 99), 30)
 
             # Put the blurred face region back into the frame image
-            frame[top:bottom, left:right] = face_image
+            small_frame[top:bottom, left:right] = face_image
 
             # Draw a box around the face
-            cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+            cv2.rectangle(small_frame, (left, top), (right, bottom), (0, 0, 255), 2)
 
             # Draw a label with a name below the face
-            cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+            cv2.rectangle(small_frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
             font = cv2.FONT_HERSHEY_DUPLEX
-            cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+            cv2.putText(small_frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
 
         # Display the resulting image
-        cv2.imshow('Debug', frame)
+        cv2.imshow('Debug', small_frame)
+
+        if names:
+            window['-OUTPUT-'].update(f"Hey, {','.join(map(str.title, names))}, what would you like to do?")
+        else:
+            window['-OUTPUT-'].update('No One Detected')
+        event, values = window.read(timeout=200)
+        if event == sg.WINDOW_CLOSED or event == 'Quit':
+            break
+
+        if names:
+            do_user(names[0])
 
         # Hit 'q' on the keyboard to quit!
         if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -91,6 +94,7 @@ def main():
 
     video_capture.release()
     cv2.destroyAllWindows()
+    window.close()
 
 from device_config import conf
 
@@ -109,8 +113,6 @@ def do_user(username):
         return
 
     response = response.split()
-    if 'quit' in response:
-        raise Exception('Quit')
     action = None
     device = None
     for dev, actions in conf.items():
